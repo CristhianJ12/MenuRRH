c0c0047097bafe84c1c6824f4d07b5325fc64513
Principal <75049277@continental.edu.pe>
2024-10-16 17:20:50 -0500

    Menu Creado

--- app/src/main/java/com/example/gestion_rrhh/MainActivity.kt
+++ app/src/main/java/com/example/gestion_rrhh/MainActivity.kt
@@ -3,19 +3,18 @@ package com.example.gestion_rrhh
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,7 +22,6 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MenuRecursosHumanos()
         }
@@ -32,7 +30,6 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MenuRecursosHumanos() {
-    // Lista de tablas como opciones de menú
     val tablas = listOf(
         "Registro del Personal",
         "Datos del Contrato",
@@ -51,13 +48,26 @@ fun MenuRecursosHumanos() {
     ) {
         Text("Menú de Recursos Humanos", fontSize = 24.sp, modifier = Modifier.padding(bottom = 16.dp))
 
-        // Iterar sobre las tablas para crear botones
-        for (tabla in tablas) {
-            OpcionMenu(tabla) {
-                // Aquí puedes gestionar la acción cuando una tabla es seleccionada
-                println("Se seleccionó: $tabla")
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier
+                    .padding(16.dp)
+                    .wrapContentHeight(Alignment.CenterVertically)
+                    .wrapContentWidth(Alignment.CenterHorizontally)
+            ) {
+                items(tablas.size) { index ->
+                    OpcionMenu(tablas[index]) {
+                        println("Se seleccionó: ${tablas[index]}")
+                    }
+                }
             }
-            Spacer(modifier = Modifier.height(8.dp)) // Espacio entre los botones
         }
     }
 }
@@ -67,10 +77,12 @@ fun OpcionMenu(tabla: String, onClick: () -> Unit) {
     Button(
         onClick = onClick,
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 8.dp)
+            .height(100.dp)
+            .fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4F934A)),
+        shape = RoundedCornerShape(8.dp)
     ) {
-        Text(text = tabla, fontSize = 18.sp)
+        Text(text = tabla, fontSize = 18.sp, color = Color.White)
     }
 }
 
